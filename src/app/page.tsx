95b8b6ca9025f55e6353be09fea7218033a9d132
Pimpinee J <pimpinee@tencent.co.th>
2024-01-30 10:58:51 +0700

    -

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { useEffect } from "react";
 import Image from "next/image";
 
 export default function Home() {
-  useEffect(() => {
-    throw new Error()
-  }, []);
+  // useEffect(() => {
+  //   throw new Error()
+  // }, []);
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
